8bc3146e0a10bfc9dbad1ae83b16bc06e65f69fe
Matheus <mtpaivamrhenrique@gmail.com>
2024-05-03 08:57:00 -0300

    test

---
+++ calculadora_gorgeta.py
@@ -0,0 +1,17 @@
+print("Calculadora de Gorgeta")
+conta = float(input("Qual foi o valor da conta? R$ "))
+gorgeta = int(input("Qual a porcentagem da gorgeta? 10, 12 ou 15?"))
+pessoas = int(input("Quantas pessoas vão dividir a conta?"))
+
+gorgeta_as_percent = gorgeta / 100
+total_gorgeta_amount = conta * gorgeta_as_percent
+total_conta = conta + total_gorgeta_amount
+conta_per_person = total_conta / pessoas
+final_amount = round(conta_per_person, 2)
+
+
+
+
+
+print(f"Cada pessoa pagará: ${final_amount}")
+
